9e9df1f7edfbc3a7932d52a0acfceaa42f027a21
jonasms <jonas.sota@gmail.com>
2016-05-06 12:28:39 -0700

    Create GroceryList WIP

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // TODO
+var groceryItems = ['kale', 'cucumber', 'cookies', 'dried mango', 'falafel'];
+
+var GroceryList = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <List items={makeList(groceryItems)}/> 
+  </div>   
+);
+
+
+
+var List = (props) => (
+  <ul>
+    {props.items}
+  </ul>
+);
+
+var Cucumber = () => (
+  <li>Cucumber</li>
+);
+
+var Cookie = () => (
+  <li>Cookie</li>
+);
+
+var GroceryListItem = (props) => {
+  var onListItemClick = (event) => {
+    console.log('clicked yo');
+  }
+  return (
+    <li onClick={onListItemClick}>{props.item}</li>
+  );
+};
+
+
+var makeList = (items) => {
+
+  return items.map(function(item) {
+    return(<GroceryListItem item={item} />);
+  });
+};
+
+  ReactDOM.render(<GroceryList />, document.getElementById("app"));
+  // ReactDOM.render(<List />, document.getElementById("list"));
