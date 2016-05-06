200a49db533068914f6776c366a1e197f175208c
jonasms <jonas.sota@gmail.com>
2016-05-06 15:11:49 -0700

    Complete GroceryList

--- app.js
+++ app.js
@@ -1,6 +1,64 @@
 // TODO
 var groceryItems = ['kale', 'cucumber', 'cookies', 'dried mango', 'falafel'];
 
+
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+
+    return (
+      <ul>
+        {this.props.items} 
+      </ul>
+    );
+  }
+}
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      done: false,
+      over: false
+    };
+  }
+
+
+  onListItemClick(event) {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  onListItemHover(event) {
+    this.setState({
+      over: true
+    });
+  }
+
+  onListItemEndHover(event) {
+    this.setState({
+      over: false
+    });
+  }
+
+  render() {
+
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.over ? 'bold' : 'normal'
+    }
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)} onMouseEnter={this.onListItemHover.bind(this)} 
+      onMouseLeave={this.onListItemEndHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
 var GroceryList = () => (
   <div>
     <h2>Grocery List</h2>
@@ -10,11 +68,11 @@ var GroceryList = () => (
 
 
 
-var List = (props) => (
-  <ul>
-    {props.items}
-  </ul>
-);
+// var List = (props) => (
+//   <ul>
+//     {props.items} 
+//   </ul>
+// );
 
 var Cucumber = () => (
   <li>Cucumber</li>
@@ -24,18 +82,16 @@ var Cookie = () => (
   <li>Cookie</li>
 );
 
-var GroceryListItem = (props) => {
-  var onListItemClick = (event) => {
-    console.log('clicked yo');
-  }
-  return (
-    <li onClick={onListItemClick}>{props.item}</li>
-  );
-};
-
+// var GroceryListItem = (props) => {
+//   var onListItemClick = (event) => {
+//     console.log('clicked yo');
+//   }
+//   return (
+//     <li onClick={onListItemClick}>{props.item}</li>
+//   );
+// };
 
 var makeList = (items) => {
-
   return items.map(function(item) {
     return(<GroceryListItem item={item} />);
   });
